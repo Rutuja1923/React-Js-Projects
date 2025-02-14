08b5e6c42e680c3c68e3565c44c6b63cdaaff141
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-14 09:37:28 +0530

    we can use React.createElement to create a new element

--- vite-project-1/src/main.jsx
+++ vite-project-1/src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -16,7 +17,16 @@ const anotherReactElement = (
   <a href='https://www.google.com/' target='_blank'>Visit Google!</a>
 )
 
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com/',
+    target: '_blank'
+  },
+  "let's go to google!",
+);
+
 createRoot(document.getElementById('root'))
 .render(
-  anotherReactElement
+  reactElement
 )
