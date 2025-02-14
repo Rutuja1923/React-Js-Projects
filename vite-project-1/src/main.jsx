45d13af5c7473ba2bfddf92c5a61dbc39040027b
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-14 09:41:24 +0530

    back to initial stage

--- vite-project-1/src/main.jsx
+++ vite-project-1/src/main.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
+//import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-function MyApp() {  //Fast refresh only works when a file has exports. Move your component(s) to a separate file.
-  return (
-    <>
-    <p>Hey there ! i am a MyApp()</p>
-    <p>I can still be used!</p>
-    </>
-  );
-}
+// function MyApp() {  //Fast refresh only works when a file has exports. Move your component(s) to a separate file.
+//   return (
+//     <>
+//     <p>Hey there ! i am a MyApp()</p>
+//     <p>I can still be used!</p>
+//     </>
+//   );
+// }
 
-const anotherReactElement = (
-  <a href='https://www.google.com/' target='_blank'>Visit Google!</a>
-)
+// const anotherReactElement = (
+//   <a href='https://www.google.com/' target='_blank'>Visit Google!</a>
+// )
 
-const reactElement = React.createElement(
-  'a',
-  {
-    href: 'https://www.google.com/',
-    target: '_blank'
-  },
-  "let's go to google!",
-);
+// const reactElement = React.createElement(
+//   'a',
+//   {
+//     href: 'https://www.google.com/',
+//     target: '_blank'
+//   },
+//   "let's go to google!",
+// );
 
 createRoot(document.getElementById('root'))
 .render(
-  reactElement
+  <StrictMode>
+    <App/>
+  </StrictMode>
 )
