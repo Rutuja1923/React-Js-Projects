e1668d9edef66bb778ad6b28c73cff130cc6040a
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-14 09:32:33 +0530

    We can define our components in the main file

--- vite-project-1/src/main.jsx
+++ vite-project-1/src/main.jsx
@@ -3,7 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyApp() {  //Fast refresh only works when a file has exports. Move your component(s) to a separate file.
+  return (
+    <>
+    <p>Hey there ! i am a MyApp()</p>
+    <p>I can still be used!</p>
+    </>
+  );
+}
+
 createRoot(document.getElementById('root'))
 .render(
+  <StrictMode>
     <App />
+    <MyApp/>
+  </StrictMode>,
 )
