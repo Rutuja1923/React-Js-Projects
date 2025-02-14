6d6871e6f92338b6641efe83bbba5a988bce1a2b
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-14 09:50:51 +0530

    Allows variables, function calls, math operations, ternary operations

--- vite-project-1/src/App.jsx
+++ vite-project-1/src/App.jsx
@@ -1,15 +1,29 @@
 import './App.css';
 import Chai from './Chai.jsx';
 
+function welcome() {
+  return "Welcome to React Learning!";
+}
+
 function App() {
-  const mentorName = 'Hitesh Choudhary'
+  const mentorName = 'Hitesh Choudhary';
+  const isLoggedIn = true;
+
   return (
     <>
       <h1>Chai aur React</h1>
-      <p>By Our Mentor {mentorName}</p>
+      <p>{`By Our Mentor ${mentorName}`}</p>
+      <p>{welcome()}</p>
+      <p>{10 + 40}</p>
       <Chai />
+      <p><p>{isLoggedIn ? "Welcome Back!" : "Please Log In"}</p></p>
     </>
   )
 }
 
 export default App
+
+/*
+In JSX, an evaluated expression is anything that JavaScript can compute and return a value for. 
+You can use JavaScript expressions inside {} in JSX, but not statements like if, for, or while.
+*/
