1d6b019929a945048925ab6be233cb193bd38dd8
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-14 11:31:32 +0530

    handling corner cases for counter value out of range (0-20)

--- simple-counter/src/App.jsx
+++ simple-counter/src/App.jsx
@@ -1,24 +1,34 @@
 import "./App.css";
-import { useState } from "react";
+import { useState} from "react";
 
 function App() {
   let [counter, setCounter] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
   
   const addValue = () => {
-    counter++;
-    console.log(`Counter Value = ${counter} | Changed at ${new Date()}`);
-    setCounter(counter);
+    if (counter >= 20) {
+      setErrorMessage("Can't increase above 20");
+      return;
+    }
+    setCounter(prevCounter => prevCounter + 1);
+    setErrorMessage("");
+    console.log(`Counter Value = ${counter} | Changed at ${new Date()}`); 
   };
   const removeValue = () => {
-    counter--;
+    if (counter <= 0) {
+      setErrorMessage("Can't reduce below 0");
+      return;
+    }
+    setCounter(prevCounter => prevCounter - 1);
+    setErrorMessage("");
     console.log(`Counter Value = ${counter} | Changed at ${new Date()}`);
-    setCounter(counter);
   };
 
   return (
     <>
       <h1>Counter</h1>
       <h2>Counter Value : {counter} </h2>
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       <button onClick={addValue}>Increase +</button>
       <button onClick={removeValue}>Decrease -</button>
     </>
