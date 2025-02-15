c3a9121021b3792c0354e092a5abb9aa89441838
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-15 20:49:54 +0530

    introduced new props

--- tailwind-props/src/main.jsx
+++ tailwind-props/src/main.jsx
@@ -1,18 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 // import App from './App.jsx'
-import Catcard from '../components/Catcard.jsx'
+import Catcard from "../components/Catcard.jsx";
 
 let myObj = {
-  age : 5,
-  favorite : "sea fish"
-}
-let myArr = ['cat', 'purr', 'milk']
+  age: 5,
+  favorite: "sea fish",
+};
+let myObj2 = {
+  age: 2,
+  favorite: "sweet corn",
+};
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Catcard name="Silky" someObj = {myObj} someArr = {myArr} />
-  </StrictMode>,
-)
+    <Catcard
+      name="Silky"
+      someObj={myObj}
+      imgSrc="https://cdn.pixabay.com/photo/2022/06/19/04/25/cat-7271017_1280.jpg"
+    />
+    <Catcard
+      name="Maxxii"
+      someObj={myObj2}
+      imgSrc="https://cdn.pixabay.com/photo/2022/07/13/16/25/cat-7319589_1280.jpg"
+    />
+  </StrictMode>
+);
