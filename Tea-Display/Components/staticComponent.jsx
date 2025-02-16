433ad2c56ea2de7bdca9ec5c473330db076b6ab5
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-16 17:05:49 +0530

    Image source props

--- Tea-Display/Components/staticComponent.jsx
+++ Tea-Display/Components/staticComponent.jsx
@@ -6,10 +6,12 @@ function StaticComponent() {
     origin: "Japan",
     description:
       "Matcha is a finely ground powdered green tea, known for its vibrant green color and rich umami flavor. It is traditionally used in Japanese tea ceremonies and is packed with antioxidants.",
+    imgSrc : "https://info.ehl.edu/hubfs/Blog-EHL-Insights/Images-EHL-Insights/EHL-Passugg_Blog_Matcha_Header.jpg"
   };
 
   return (
     <div className="tea-container">
+      <img src={tea.imgSrc} alt={`${tea.name}`} className="tea-image" />
       <h2 className="tea-name">{tea.name}</h2>
       <p className="tea-origin">
         <strong>Origin:</strong> {tea.origin}
