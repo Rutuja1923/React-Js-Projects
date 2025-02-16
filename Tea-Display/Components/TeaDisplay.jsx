f120fc7bebe484aadbd3ddbf1dca6647dbc7cbb2
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-16 17:07:28 +0530

    tea card handling

--- Tea-Display/Components/TeaDisplay.jsx
+++ Tea-Display/Components/TeaDisplay.jsx
@@ -1,10 +1,11 @@
 import "./TeaDisplay.css";
 import PropTypes from "prop-types";
 
-function TeaDisplay({name, origin, description}) {
+function TeaDisplay({name, origin, description, imgSrc}) {
 
   return (
     <div className="tea-container">
+      <img src={imgSrc} alt={`${name}`} className="tea-image" />
       <h2 className="tea-name">{name}</h2>
       <p className="tea-origin">
         <strong>Origin:</strong> {origin}
@@ -20,4 +21,5 @@ TeaDisplay.propTypes = {
   name: PropTypes.string.isRequired,
   origin: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
 };
