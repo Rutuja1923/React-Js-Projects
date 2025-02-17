92a1a7559e33936b14551d7822226b4cb77aa2f4
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-17 20:29:27 +0530

    Trying new component

--- vite-project-1/src/App.jsx
+++ vite-project-1/src/App.jsx
@@ -1,29 +1,14 @@
 import './App.css';
-import Chai from './Chai.jsx';
-
-function welcome() {
-  return "Welcome to React Learning!";
-}
+import EvaluatedExpressions from './EvaluatedExpressions';
 
 function App() {
-  const mentorName = 'Hitesh Choudhary';
-  const isLoggedIn = true;
 
   return (
     <>
-      <h1>Chai aur React</h1>
-      <p>{`By Our Mentor ${mentorName}`}</p>
-      <p>{welcome()}</p>
-      <p>{10 + 40}</p>
-      <Chai />
-      <p><p>{isLoggedIn ? "Welcome Back!" : "Please Log In"}</p></p>
+      <EvaluatedExpressions/>
     </>
   )
 }
 
 export default App
 
-/*
-In JSX, an evaluated expression is anything that JavaScript can compute and return a value for. 
-You can use JavaScript expressions inside {} in JSX, but not statements like if, for, or while.
-*/
