d2c91a857fc2592e1263be4deb0533c2b05530ab
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-19 20:12:23 +0530

    Testing API user profile component

--- User-Profile/src/App.jsx
+++ User-Profile/src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
-import UserProfile from "./components/UserProfile";
+import APIUserProfile from "./components/APiUserProfile";
+// import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
-      <UserProfile
+      {/* <UserProfile
         name={"Rutuja"}
         age={21}
         bio={
           "An open-minded and empathetic soul who values growth, kindness, and making meaningful connections in life."
         }
-      />
+      /> */}
+      <APIUserProfile />
     </>
   );
 }
