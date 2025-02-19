213d2fea5664c462c6fb6b28059c0da6ad7c35cf
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-19 18:50:27 +0530

    passing props

--- User-Profile/src/App.jsx
+++ User-Profile/src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
-import UserProfile from './components/UserProfile'
+import "./App.css";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-
   return (
     <>
-      <UserProfile name = {"Rutuja"} age = {21} bio = {"An open-minded and empathetic soul who values growth, kindness, and making meaningful connections in life."}/>
+      <UserProfile
+        name={"Rutuja"}
+        age={21}
+        bio={
+          "An open-minded and empathetic soul who values growth, kindness, and making meaningful connections in life."
+        }
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
