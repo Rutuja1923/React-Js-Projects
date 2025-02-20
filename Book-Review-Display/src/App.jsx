c4d48c8683ec905de18415f8c7fe2b8644ffa9ca
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-20 06:49:34 +0530

    Testing dynamic book review component

--- Book-Review-Display/src/App.jsx
+++ Book-Review-Display/src/App.jsx
@@ -2,9 +2,16 @@ import './App.css'
 import BookReview from './components/BookReview'
 
 function App() {
+  const book1 = {
+    name : "The Fates Divide",
+    author : "Vernica Roth",
+    rating : "4.5",
+    review : "The Fates Divide by Veronica Roth is a gripping sequel to Carve the Mark, deepening the complexities of fate, power, and identity. With rich world-building and evolving character dynamics, the novel explores destiny's fluidity and the weight of choices. Though pacing issues arise at times, Roth delivers an emotionally charged narrative that keeps readers engaged till the end.",
+    imgSrc : "https://veronicarothbooks.com/wp-content/uploads/2016/04/ctm-homepage-198x300.png",
+  }
   return (
     <>
-      <BookReview />
+      <BookReview name={book1.name} author={book1.author} rating={book1.rating} review={book1.review} imgSrc={book1.imgSrc}/>
     </>
   )
 }
