79472eafe7558a5996b1a27a64c1c2601b95efa8
Rutuja1923 <rutujajirage19@gmail.com>
2025-02-20 06:49:01 +0530

    Dynamic component using props

--- Book-Review-Display/src/components/BookReview.jsx
+++ Book-Review-Display/src/components/BookReview.jsx
@@ -5,24 +5,19 @@ function BookReview({name, author, rating, review, imgSrc}) {
   return (
     <div className="book-review">
       <img
-        src="https://veronicarothbooks.com/wp-content/uploads/2016/04/ctm-homepage-198x300.png"
-        alt="book-cover-page"
+        src={imgSrc}
+        alt={`${name}-cover`}
         className="book-cover"
       />
       <div className="book-details">
-        <h2>The Fates Divide</h2>
-        <h3>By Veronica Roth</h3>
+        <h2>{name}</h2>
+        <h3>By {author}</h3>
         <p className="rating">
-          <span className="fa fa-star checked"></span> Rating : 4.5/5
+          <span className="fa fa-star checked"></span> Rating : {rating}/5
         </p>
         <p className="review">
           <strong>Review :&nbsp;</strong>
-          The Fates Divide by Veronica Roth is a gripping sequel to Carve the
-          Mark, deepening the complexities of fate, power, and identity. With
-          rich world-building and evolving character dynamics, the novel
-          explores destiny&apos;s fluidity and the weight of choices. Though
-          pacing issues arise at times, Roth delivers an emotionally charged
-          narrative that keeps readers engaged till the end.
+          {review}
         </p>
       </div>
     </div>
